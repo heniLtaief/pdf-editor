51473e04ba7c416c573c584145248d8a4e6b2c31
heni_ltaief <heniltaief@gmail.com>
2023-12-19 12:25:26 +0100

    fixing page layout responsiveness & advancing on the right side of edit pdf page

--- pdf-editor/src/App.jsx
+++ pdf-editor/src/App.jsx
@@ -5,47 +5,59 @@ import Logo from "./assets/logo.png";
 function App() {
   return (
     <>
-      <div className="grid grid-cols-4 h-full w-full">
+      <section className="min-h-screen flex items-stretch text-white ">
         <div
-          className="background relative flex items-center col-span-2 object-fit w-full h-screen"
+          className="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center"
           style={{
             backgroundImage: `url(${BgImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
           }}
         >
           <img
             src={Logo}
             alt="logo"
-            className="absolute w-32 h-10 left-12 top-6"
+            className="absolute w-44 h-10 left-10 top-6"
           />
-          <div className="textContainer grid grid-cols-4 ">
+          <div className="w-full z-10 grid grid-cols-4 gap-10">
             <p className="leftText col-span-1 m-auto pb-64 uppercase text-white -rotate-90 w-max h-12">
               TECHNICAL TEST FRO FRENTEND DEVELOPER
             </p>
             <div className="rightText col-span-3 flex flex-col gap-12">
-              <p className="heading text-6xl text-white">
+              <h1 className="text-5xl text-left tracking-wide">
                 All your services, No-code.
-              </p>
-              <p className="subHeading text-white text-lg">
+              </h1>
+              <p className="text-3xl my-4">
                 Build your business apps and automate your tasks without coding.
               </p>
-              <button className="editBtn w-64 h-12 bg-transparent text-white rounded-full border-2 border-color-white hover:bg-white hover:text-[#5068F2]">
+              <button className="editBtn w-64 h-12 bg-transparent text-lg text-white font-bold rounded-full border-2 border-color-white hover:bg-white hover:text-[#5068F2]">
                 Edit a PDF
               </button>
             </div>
           </div>
         </div>
-        <div className="edit col-span-2 ">
-          <div className="editText flex justify-center items-center h-screen w-full gap-1">
-            <p className="title text-black text-md">
-              TECHNICAL TEST FRO FRENTEND DEVELOPER
+        <div
+          className="lg:w-1/2 w-full flex flex-col items-center justify-center text-center md:px-6 px-0 z-0 relative"
+          style={{ backgroundColor: "#ffff" }}
+        >
+          <div className="absolute mx-auto top-10 z-20 text-center w-full">
+            <h1 className="rightTitle text-4xl text-white lg:text-black">
+              PDF Editor
+            </h1>
+            <p className="py-6 space-x-2 text-white lg:text-black text-xl">
+              Edit PDF files. Fill & sign PDF
             </p>
-            <span className="edit text-md text-[#5068F2]">Edit a PDF</span>
+          </div>
+          <div className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center">
+            <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+          </div>
+
+          <div className="editText flex flex-col md:flex-row justify-center items-center gap-1 w-full py-6 z-20">
+            <p className="title lg:text-black text-md text-white">
+              TECHNICAL TEST FOR FRONTEND DEVELOPER
+            </p>
+            <span className="edit text-md text-[#5068F2] hover:underline cursor-pointer">Edit a PDF</span>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
